de60bb57ce668b367a549216ba428c24a170d822
XfHua <jasonHuaxf@163.com>
2020-03-09 09:54:05 +0800

    Add files via upload

---
+++ App164/App164/App164.Android/CustomEditorRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using App164;
+using App164.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEditor), typeof(CustomEditorRenderer))]
+namespace App164.Droid
+{
+    class CustomEditorRenderer : EditorRenderer
+    {
+        public CustomEditorRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.LightGreen);
+            }
+        }
+    }
+}
---
+++ App164/App164/App164/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App164
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var st = new StackLayout();
+            st.Children.Add(new CustomEditor());
+
+            Content = st;
+        }
+    }
+    public class CustomEditor : Editor
+    {
+
+    }
+}
